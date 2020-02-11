af6eda7b61c8d0bae15ba28d3517b99b8013875d
Kyle Mikolajczyk <dctm1234@gmail.com>
2020-02-11 10:56:12 -0500

    Tests updated and should be all done

--- Tests/procAncestry.c
+++ Tests/procAncestry.c
@@ -33,6 +33,16 @@ int main (int argc, char *argv[]) {
 	printf("The return values of the system calls are:\n");
 
 	struct ancestry *myAncestry = malloc(sizeof(struct ancestry));
+	//Here we need to initialize all pid_t to zero
+	for(int i = 0; i < 100; i++)
+	{
+		myAncestry->siblings[i] = -1;
+		myAncestry->children[i] = -1;
+	}
+	for(int i = 0; i < 10; i++)
+	{
+		myAncestry->ancestors[i] = -1;
+	}
 	if(argc < 2)
 	{
 		printf("Not enough arguments! Exiting...\n");
@@ -42,5 +52,23 @@ int main (int argc, char *argv[]) {
 	unsigned short* pid_ptr = &pid;
 	// unsigned short *pid = malloc(sizeof(unsigned short));
 	printf("\tcs3013_syscall2: %ld\n", testCall2(pid_ptr, myAncestry));
+	printf("Ancestors that were found:\n");
+	for(int i = 0; i < 10; i++)
+	{
+		if(myAncestry->ancestors[i] == -1 ){break;}
+		printf("%d\n", myAncestry->ancestors[i]);
+	}
+	printf("Siblings that were found:\n");
+	for(int i = 0; i < 100; i++)
+	{
+		if(myAncestry->siblings[i] == -1 ){break;}
+		printf("%d\n", myAncestry->siblings[i]);
+	}
+	printf("Children that were found:\n");
+	for(int i = 0; i < 100; i++)
+	{
+		if(myAncestry->children[i] == -1 ){break;}
+		printf("%d\n", myAncestry->children[i]);
+	}
 	return 0;
 }
